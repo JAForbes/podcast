a989763ac8039132da2536fa3f02b3f382c7b13e
James <james.a.forbes@gmail.com>
2017-10-13 01:40:20 +1100

    Working prototype

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const m = require('mithril')
 const stream = require('mithril/stream')
 const sst = require('static-sum-type/configs/dev/foldThrow.js')
 const yslashn = require('static-sum-type/modules/yslashn')
-const actions$ = stream()
+
 
 const encase = ({Y,N}) => f => (...xs) => {
     try {
@@ -12,42 +12,33 @@ const encase = ({Y,N}) => f => (...xs) => {
     }
 }
 
-const Maybe = yslashn.maybe('Maybe')
-    Maybe.encase = encase(Maybe)
 
-Maybe.map = sst.map( Maybe )
-Maybe.chain = f => fold(Maybe)({
-    Y: (a) => f(a).value
-    ,N: Maybe.N
-})
+const Model = (sideEffects, initialModel, actions$) => 
+    stream.scan(function(p, n){
 
-const querySelector = Maybe.encase( 
-    (node, selector) => node.querySelector(selector)
-)
+        sideEffects(actions$, p,n)
 
-const model$ = stream.scan(function(p, n){
-
-    DOMSideEffects(p,n)
-
-    if( n.type == 'UPDATE_FEED_VALUE' ){
-        return {
-            feedurl: n.value
-            ,feedraw: null
-            ,feedparsed: null
+        if( n.type == 'UPDATE_FEED_VALUE' ){
+            return {
+                feedurl: n.value
+                ,feedraw: null
+                ,feedparsed: null
+            }
+        } else if ( n.type == 'FETCH_FEED' ){
+            return p
+        } else if ( n.type == 'PARSE_FEED' ){
+            return p
+        } else {
+            return p
         }
-    } else if ( n.type == 'FETCH_FEED' ){
-        return p
-    } else if ( n.type == 'PARSE_FEED' ){
-        return p
-    } else {
-        return p
-    }
 
-    return p
-}, { feedurl: '', feedraw: null, feedparsed: null }, actions$)
+        return p
 
-function DOMSideEffects(p, n){
+    }, initialModel, actions$)
+
+function DOMSideEffects(update, p, n){
     
+    // eslint-disable-next-line fp/no-mutation, no-undef
     window.state = p
 
     if( n.type == 'FETCH_FEED' ){
@@ -57,13 +48,16 @@ function DOMSideEffects(p, n){
             ,method: 'GET'
         })
             .then(function(response){
+                
+                // eslint-disable-next-line fp/no-mutation
                 p.feedraw = response
-                actions$({
+                update({
                     type: 'PARSE_FEED'
                 })
             })
     } else if (n.type == 'PARSE_FEED'){
 
+        // eslint-disable-next-line no-undef
         const xml = new DOMParser().parseFromString(
             p.feedraw
                 .replace(/itunes:/g, 'itunes_')
@@ -86,12 +80,12 @@ function DOMSideEffects(p, n){
             ,items:
                 xmlItems.map(function(xml){
                     return {
-                        title: (xml.querySelector('description') || defaultNode).innerHTML
+                        title: (xml.querySelector('title') || defaultNode).innerHTML
+                        ,itunesSubtitle: (xml.querySelector('itunes_subtitle') || defaultNode).innerHTML
                         ,description: (xml.querySelector('description') || defaultNode).innerHTML
                         ,pubDate: (xml.querySelector('pubDate') || defaultNode).innerHTML
                         ,itunesAuthor: (xml.querySelector('itunes_author') || defaultNode).innerHTML
                         ,itunesTitle: (xml.querySelector('itunes_title') || defaultNode).innerHTML
-                        ,itunesEpisodeType: (xml.querySelector('itunes_subtitle') || defaultNode).innerHTML
                         ,itunesDuration: (xml.querySelector('itunes_duration') || defaultNode).innerHTML
                         ,itunesExplicit: (xml.querySelector('itunes_explicit') || defaultNode).innerHTML
                         ,enclosure: {
@@ -103,35 +97,83 @@ function DOMSideEffects(p, n){
                 })
         }
 
-        state.feedparsed = channel  
+        // eslint-disable-next-line fp/no-mutation
+        p.feedparsed = channel  
 
     }
 }
 
 
 
-const view$ = model$.map(function(model){
-    return m(
+const View = update => model =>
+    m(
         'div'
         , m('input'
             , { oninput: 
                 m.withAttr('value', function(value){
-                    actions$({
-                        type: 'UPDATE_FEED_VALUE',
-                        value
+                    update({
+                        type: 'UPDATE_FEED_VALUE'
+                        ,value
                     })
                 })
+                ,value: model.feedurl
             }
         )
         , m('button', {
             onclick(){
-                actions$({
+                update({
                     type: 'FETCH_FEED'
                 })
             }
         }, 'Fetch')
-        ,m('p', 'https://feeds.feedburner.com/InterceptedWithJeremyScahill')
-    )
-})  
+        , model.feedparsed
+        && [
+            m('a', { href: model.feedparsed.link }
+                ,m('h1', model.feedparsed.title)
+            )
+            ,m('img', { 
+                src: model.feedparsed.itunesImageHref
+                ,style: { maxWidth: '100px' }
+            })
+            ,m('subheading', model.feedparsed.description)
+            ,model.feedparsed.items.map(function(item){
+                return [
+                    m('heading', item.title)
+                    ,m('subheading', item.itunesSubtitle)
+                    ,m('p', item.description)
+                    ,m('audio'
+                        ,{ controls: 'controls'  
+                        }
+                        ,m('source', {
+                            src: item.enclosure.url
+                        })
+                    )
 
-view$.map( x => m.render(document.body, x) )
+                ]
+            })
+            ,m('pre', JSON.stringify(model.feedparsed, null, 2))
+        ]
+    )
+
+function Component(actions$, update, initialModel){
+        
+    return Model(
+        DOMSideEffects
+        , initialModel
+        , actions$
+    )
+    .map( View(update) )
+}
+
+const actions$ = stream()
+
+Component( 
+    actions$
+    , actions$
+    ,{ feedurl: 'https://feeds.feedburner.com/InterceptedWithJeremyScahill'
+    , feedraw: null
+    , feedparsed: null 
+    } 
+)
+    // eslint-disable-next-line no-undef
+    .map( x => m.render(document.body, x) )
